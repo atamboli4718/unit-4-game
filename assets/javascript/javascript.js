f3bf12fd94372cbfd33f426e4affafa68568ccc8
Alicia Tamboli <atamboli4718@gmail.com>
2019-05-12 13:17:34 -0400

    got the random numbers generating and number one pushing to the array correctly

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,13 @@
 /* Logic thought out:
 
-create a var for each crystal (crystal1, crystal2, etc)
-create a var for the 4 options the computer can guess from to assign to each crystal 1-12 (options ??)
-create a var for the target numbers for the computer to guess from 19-120 (targetNumber)
-create a function to randomly generat 4 numbers to put in crystal vars
-create a function to randomly generate the target number
-create a var for the user to add to when they click on each crystal (userGuess)
+^create a var for each crystal (crystal1, crystal2, etc)
+^create a var for the 4 options the computer can guess from to assign to each crystal 1-12 (options ??)
+^create a var for the target numbers for the computer to guess from 19-120 (targetNumber)
+^create a function to randomly generat 4 numbers to put in crystal vars
+^create a function to randomly generate the target number
+^create a var for the user to add to when they click on each crystal (userGuess)
     disply ^ on the DOM
+create a function for when the user clicks on each each crystal to add to the userGuess var    
 create a var for wins (wins)
 create an alert for when targetOption == clicks, user wins! adds that win to the wins var above
     rerun function to generation the 4 numbers and the target numbers
@@ -17,26 +18,49 @@ create a function for if the userGuess > targetNumber   then
 
 */
 
-$(document).ready(function() {
+
+        var crystal1 = $("#crystal1");
+        var crystal2 = $("#crystal2");
+        var crystal3 = $("#crystal3");
+        var crystal4 = $("#crystal4");
+
+        var targetNumber = $("#targetNumber");
+
+        //function for generating random numbers for crystals
+        crystal1 = Math.floor(Math.random() * 12) + 1 ;
+            console.log(crystal1);
+        crystal2 = Math.floor(Math.random() * 12) + 1 ;
+            console.log(crystal2);
+        crystal3 = Math.floor(Math.random() * 12) + 1 ;
+            console.log(crystal3);
+        crystal4 = Math.floor(Math.random() * 12) + 1 ;
+            console.log(crystal4);
+
+        //function to randomly generate target number
+        targetNumber = Math.floor(Math.random() * 120) + 19;
+            console.log(targetNumber);
+
+    //var for guesses to increment into
+    var yourGuess = [] ;
+
+     //functions for clicks to increment into yourGuess var
+    $("#crystal1").on("click", function () {
+        yourGuess.push(crystal1);
+        console.log("User guessed: " + crystal1);
+        console.log(yourGuess);
+        });
+
+
+
+
+
+
+
+
+
+
+
     
-    var $crystal1 = $("#crystal1");
-    var $crystal2 = $("#crystal2");
-    var $crystal3 = $("#crystal3");
-    var $crystal4 = $("#crystal4");
-    var $targetNumber = $("#targetNumber");
+    
 
-    //function for generating random numbers for crystals
-    $crystal1 = Math.floor(Math.random() * 12) + 1 ;
-        console.log($crystal1);
-    $crystal2 = Math.floor(Math.random() * 12) + 1 ;
-        console.log($crystal2);
-    $crystal3 = Math.floor(Math.random() * 12) + 1 ;
-        console.log($crystal3);
-    $crystal4 = Math.floor(Math.random() * 12) + 1 ;
-        console.log($crystal4);
 
-    //function to randomly generate target number
-    $targetNumber = Math.floor(Math.random() * 120) + 19 ;
-    console.log($targetNumber);
-
-})
