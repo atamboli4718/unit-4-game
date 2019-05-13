44c3f3e179247b835f6d3785e89a6a85895d081c
Alicia Tamboli <atamboli4718@gmail.com>
2019-05-12 20:58:58 -0400

    finished html and fixed number generator to sum correctly

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,23 +1,5 @@
-/* Logic thought out:
 
-^create a var for each crystal (crystal1, crystal2, etc)
-^create a var for the 4 options the computer can guess from to assign to each crystal 1-12 (options ??)
-^create a var for the target numbers for the computer to guess from 19-120 (targetNumber)
-^create a function to randomly generat 4 numbers to put in crystal vars
-^create a function to randomly generate the target number
-^create a function for when the user clicks on each each crystal to add to the sum var    
-create a var for the user to add to when they click on each crystal (userGuess)
-    disply ^ on the DOM
-^create a var for wins (wins)
-^create an alert for when targetOption == clicks, user wins! adds that win to the wins var above
-    rerun function to generation the 4 numbers and the target numbers
-^create a function for if the userGuess > targetNumber   then
-    alert "you loose!"
-    rerun function to generate #'s 
-    wins return to 0 and game starts over
-
-*/
-
+//set up neccesary vars with global scope
 var sum = 0;
 var wins = 0;
 var crystal1;
@@ -26,20 +8,21 @@ var crystal3;
 var crystal4;
 var targetNumber;
 
-
+//function to reset the wins only - yes I have to break them out!
 function resetWins() {
     wins = 0; 
     $("#wins").html(wins); //written here to reset the 0 on the DOM
 };
+//function to reset the sum only - yes I have to break them out!
 function resetSum () {
     sum = 0;
     $("#userGuess").html(sum); //written here to reset the 0 on the DOM
 }
 
-
+//function to setup the crystals and the target number values
 function setUp() {
 
-    //function for generating random numbers for crystals
+    //functions for generating random numbers for crystals
     crystal1 = Math.floor(Math.random() * 12) + 1;
     console.log(crystal1);
     crystal2 = Math.floor(Math.random() * 12) + 1;
@@ -49,10 +32,22 @@ function setUp() {
     crystal4 = Math.floor(Math.random() * 12) + 1;
     console.log(crystal4);
 
-    //function to randomly generate target number
-    targetNumber = Math.floor(Math.random() * 120) + 19;
+    //functions to create variables (a,b,c,d) for the targetNumber generator below
+    var a = Math.floor(Math.random()*2) + 1;
+    console.log("a is: "+a)
+    var b = Math.floor(Math.random()*3) + 1;
+    console.log("b is: "+b)
+    var c = Math.floor(Math.random()*3) + 1;
+    console.log("c is: "+c)
+    var d = Math.floor(Math.random()*2) + 1;
+    console.log("d is: "+d)
+
+
+    //function to generate target number 
+    targetNumber = a*crystal1 + b*crystal2 + c*crystal3 + d*crystal4
     console.log(targetNumber);
-    $("#targetNumber").html(targetNumber);
+    $("#targetNumber").html(targetNumber); 
+
 }
 
 //click functions for clicks to increment into sum var
@@ -61,7 +56,7 @@ $("#crystal1").on("click", function () {
     console.log("User guessed: " + crystal1);
     console.log("Total user guess thus far: " + sum);
     $("#userGuess").html(sum);
-    sumCheck(); //sum and target are put here because they are scoped inside of the setUp function and need to be referenced in the sumCheck function
+    sumCheck(); 
 });
 
 $("#crystal2").on("click", function () {
@@ -87,6 +82,7 @@ $("#crystal4").on("click", function () {
     sumCheck();
 });
 
+//calls set up function
 setUp();
 
 //function to compare sum var to wins var
